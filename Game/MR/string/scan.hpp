4d4636a1396c1b3596968e0d8727f3f84c9cd0dc
riidefi <34194588+riidefi@users.noreply.github.com>
2019-08-14 06:52:32 -0400

    :art: [MR] Cleaned up string headers.

--- Game/MR/string/scan.hpp
+++ Game/MR/string/scan.hpp
@@ -6,7 +6,6 @@
 
 namespace MR {
 
-#if 0
 #define MR_SCAN_DECL_EX(name, type, suffix) \
 	void scan##name##suffix(char*, char*, type*);
 
@@ -16,16 +15,15 @@ namespace MR {
 #define MR_SCAN_VEC4_DECL(type) \
 	MR_SCAN_DECL_EX(type, type, x4)
 
-MR_SCAN_DECL(32, s32);
-MR_SCAN_DECL(16, u16);
-MR_SCAN_DECL(8, u8);
-MR_SCAN_DECL(f32, f32);
+MR_SCAN_DECL(32, s32)
+MR_SCAN_DECL(16, u16)
+MR_SCAN_DECL(8, u8)
+MR_SCAN_DECL(f32, f32)
 
 // Vector scanners
 
-MR_SCAN_VEC4_DECL(u8);
-MR_SCAN_VEC4_DECL(f32);
-MR_SCAN_VEC4_DECL(f32);
-#endif
+MR_SCAN_VEC4_DECL(u8)
+MR_SCAN_VEC4_DECL(f32)
+MR_SCAN_VEC4_DECL(f32)
 
 } // namespace MR
