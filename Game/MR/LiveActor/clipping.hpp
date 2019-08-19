f49e032993dbba59a6b3c2d0c448b78bc8b23068
riidefi <34194588+riidefi@users.noreply.github.com>
2019-08-19 13:43:18 -0400

    :sparkles: [MR/LiveActor] Actor clipping utility.

--- Game/MR/LiveActor/clipping.hpp
+++ Game/MR/LiveActor/clipping.hpp
@@ -1,27 +1,84 @@
 #pragma once
 
 #include "smg_types.h"
+#include <JSystem/JGeometry/TVector.hpp>
 
 class LiveActor;
 class JMapInfoIter;
 
 namespace MR {
 
-// TODO: CONST WRONG
-void setClippingTypeSphere(LiveActor *param_1,float param_2);
-//void setClippingTypeSphere(LiveActor *param_1,float param_2,TVec3<float> *param_3);
-void setClippingTypeSphereContainsModelBoundingBox(LiveActor *param_1,float param_2);
-void setClippingFar50m(LiveActor *param_1);
-void setClippingFar100m(LiveActor *param_1);
-void setClippingFar200m(LiveActor *param_1);
-void setClippingFar300m(LiveActor *param_1);
-void setClippingFarMax(LiveActor *param_1);
-void setClippingFar(LiveActor *param_1,float param_2);
-void setGroupClipping(LiveActor *param_1,JMapInfoIter *param_2,int param_3);
-void validateClipping(LiveActor *param_1);
-void invalidateClipping(LiveActor *param_1);
-unsigned int changeShowModelFlagSyncNearClipping(LiveActor *param_1,float param_2);
-unsigned int isClipped(LiveActor *param_1);
-unsigned int isInvalidClipping(LiveActor *param_1);
+//! @brief Configure spherical clipping.
+//!
+//! @param[in] pActor  The concerning actor.
+//! @param[in] radius  Radius of the sphere.
+//!
+void setClippingTypeSphere(LiveActor* pActor, f32 radius);
+
+//! @brief Configure spherical clipping.
+//!
+//! @param[in] pActor  The concerning actor.
+//! @param[in] radius  Radius of the sphere.
+//! @param[in] pVec    Origin?
+//!
+void setClippingTypeSphere(LiveActor* pActor, f32 radius, const JGeometry::TVec3f* pVec);
+
+//! @brief Configure spherical clipping to encompass the actor bounding box.
+//!
+//! @param[in] pActor  The concerning actor.
+//! @param[in] pad     Bias to the bounding sphere radius.
+//!
+void setClippingTypeSphereContainsModelBoundingBox(LiveActor* pActor, f32 pad);
+
+//! @brief Configure the actor far clipping level radius to 50 meters.
+//!
+//! @param[in] pActor The concerning actor.
+//!
+void setClippingFar50m(LiveActor* pActor);
+
+//! @brief Configure the actor far clipping level radius to 100 meters.
+//!
+//! @param[in] pActor The concerning actor.
+//!
+void setClippingFar100m(LiveActor* pActor);
+
+//! @brief Configure the actor far clipping level radius to 200 meters.
+//!
+//! @param[in] pActor The concerning actor.
+//!
+void setClippingFar200m(LiveActor* pActor);
+
+//! @brief Configure the actor far clipping level radius to 300 meters.
+//!
+//! @param[in] pActor The concerning actor.
+//!
+void setClippingFar300m(LiveActor* pActor);
+
+// NOTE: Higher far clipping level setters almost certainly linker stripped
+
+//! @brief Configure the actor far clipping level radius to the maximum distance.
+//!
+//! @param[in] pActor The concerning actor.
+//!
+void setClippingFarMax(LiveActor* pActor);
+
+//! @brief Configure the actor far clipping level radius to the specified distnace.
+//!
+//! @param[in] pActor   The concerning actor.
+//! @param[in] distance The distance to set. Must be one of the following, or no action will be taken:
+//!				- Set distance level to equivalent: 600, 500, 400, 300, 200, 10, 50
+//!				- Set distance level to maximum: -1
+//!
+void setClippingFar(LiveActor* pActor, float distance);
+
+void setGroupClipping(LiveActor* pActor, JMapInfoIter* pIter, int groupId);
+
+void validateClipping(LiveActor* pActor);
+void invalidateClipping(LiveActor* pActor);
+
+bool changeShowModelFlagSyncNearClipping(LiveActor* pActor, f32 b);
+
+bool isClipped(const LiveActor* pActor);
+bool isInvalidClipping(const LiveActor* pActor);
 
 } // namespace MR
--- Game/Util/LiveActor/LiveActorUtil.cpp
+++ Game/Util/LiveActor/LiveActorUtil.cpp
@@ -5,9 +5,138 @@
 
 #include "LiveActorUtil.hpp"
 #include "Game/LiveActor/LiveActor/LiveActor.hpp"
+#include "Game/MR/ClippingUtil.hpp"
 
 namespace MR {
 
+#pragma region Clipping
+// clipping.hpp
+
+void setClippingTypeSphere(LiveActor* pActor, f32 radius)
+{
+	// ClippingActorHolder
+	getClippingDirector()->_10->setTypeToSphere(pActor, radius, nullptr);
+}
+
+void setClippingTypeSphere(LiveActor* pActor, f32 radius, const JGeometry::TVec3f* pVec)
+{
+	getClippingDirector()->_10->setTypeToSphere(pActor, radius, pVec);
+}
+
+void setClippingTypeSphereContainsModelBoundingBox(LiveActor* pActor, f32 pad)
+{
+	f32 boundingRadius = 0.0f;
+	calcModelBoundingRadius(&boundingRadius, pActor);
+	getClippingDirector()->_10->setTypeToSphere(pActor, boundingRadius + pad, nullptr);
+}
+#pragma endregion
+
+enum eFarClipLevel
+{
+	FAR_CLIP_LEVEL_MAX, // 0
+	FAR_CLIP_LEVEL_600M, // 1
+	FAR_CLIP_LEVEL_500M, // 2
+	FAR_CLIP_LEVEL_400M, // 3
+	FAR_CLIP_LEVEL_300M, // 4
+	FAR_CLIP_LEVEL_200M, // 5
+	FAR_CLIP_LEVEL_100M, // 6
+	FAR_CLIP_LEVEL_50M, // 7
+};
+
+void setClippingFar50m(LiveActor* pActor)
+{
+	getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_50M);
+}
+void setClippingFar100m(LiveActor* pActor)
+{
+	getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_100M);
+}
+void setClippingFar200m(LiveActor* pActor)
+{
+	getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_200M);
+}
+void setClippingFar300m(LiveActor* pActor)
+{
+	getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_300M);
+}
+
+void setClippingFarMax(LiveActor* pActor)
+{
+	getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_MAX);
+}
+
+void setClippingFar(LiveActor* pActor, float distance)
+{
+	switch (static_cast<int>(distance))
+	{
+	case -1:
+		getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_MAX);
+		break;
+	case 50:
+		getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_50M);
+		break;
+	case 100:
+		getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_100M);
+		break;
+	case 200:
+		getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_200M);
+		break;
+	case 300:
+		getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_300M);
+		break;
+	case 400:
+		getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_400M);
+		break;
+	case 500:
+		getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_500M);
+		break;
+	case 600:
+		getClippingDirector()->_10->setFarClipLevel(pActor, FAR_CLIP_LEVEL_600M);
+		break;
+	}
+}
+
+void setGroupClipping(LiveActor* pActor, JMapInfoIter* pIter, int groupId)
+{
+	getClippingDirector()->joinToGroupClipping(pActor, pIter, groupId);
+}
+
+void validateClipping(LiveActor* pActor)
+{
+	if (pActor->mFlag->_08)
+		getClippingDirector()->validateClipping(pActor);
+}
+
+void invalidateClipping(LiveActor* pActor)
+{
+	if (pActor->mFlag->_08)
+		getClippingDirector()->invalidateClipping(pActor);
+	else if (pActor->mFlag->_07)
+		pActor->endClipped();
+}
+
+bool changeShowModelFlagSyncNearClipping(LiveActor* pActor, f32 b)
+{
+	if (isJudgedToNearClip(pActor->mTranslation, b))
+	{
+		hideModelAndOnCalcAnimIfShown(pActor);
+		return 0;
+	}
+	else
+	{
+		showModelIfHidden(pActor);
+	}
+}
+
+bool isClipped(const LiveActor* pActor)
+{
+	return pActor->mFlag._07;
+}
+
+bool isInvalidClipping(const LiveActor* pActor)
+{
+	return pActor->mFlag._08;
+}
 
 #pragma region Action
 // action.hpp
