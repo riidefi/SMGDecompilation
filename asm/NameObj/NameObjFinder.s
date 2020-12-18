616beb6ba8a6d71ad8b49863535eb55b3a534104
riidefi <34194588+riidefi@users.noreply.github.com>
2020-12-18 18:53:00 -0500

    :sparkles: Added remaining assembly files

---
+++ asm/NameObj/NameObjFinder.s
@@ -0,0 +1,22 @@
+; Generated with ikazuchi 1.0 by riidefi
+; Object File: NameObjFinder
+; Segments:
+;     .text:       0x8026bd84 -> 0x8026bd98
+
+
+; Symbols Defined
+; 8026bd84 -> 8026bd98 find__13NameObjFinderFPCc
+
+
+; Exports
+.global find__13NameObjFinderFPCc
+
+
+; Segments
+.section .text
+find__13NameObjFinderFPCc:
+.byte 0x80, 0xAD, 0xC5, 0xC8 # 8026bd84
+.byte 0x7C, 0x64, 0x1B, 0x78 # 8026bd88
+.byte 0x80, 0x65, 0x00, 0x24 # 8026bd8c
+.byte 0x80, 0x63, 0x00, 0xA8 # 8026bd90
+.byte 0x48, 0x00, 0x03, 0xB8 # 8026bd94
